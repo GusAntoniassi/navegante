be2e6601a400a7c060d913eff610341dcc2dbcf1
Gustavo <gus.antoniassi@gmail.com>
2020-04-10 07:36:01 -0300

    Implement a very basic Container component implementation
    It's reeeeeeally ugly, I know

--- src/web/react/src/App.tsx
+++ src/web/react/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Header } from './components/ui/Header';
 import { Sidebar } from './components/ui/Sidebar';
+import { Container } from './components/containers/Container';
+
+import { Container as EntityContainer } from 'core';
+const container = new EntityContainer();
+container.id = "abc123456";
+container.name = "foobar";
+container.ports = ["80:8000/TCP"];
+container.volumes = ["/var/lib/data", "/home/foo/bar:/usr/local/application"];
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Sidebar/>
+      <Container container={container}/>
     </div>
   );
 }
--- src/web/react/src/components/containers/Container/index.tsx
+++ src/web/react/src/components/containers/Container/index.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
 
+import { Container as EntityContainer } from 'core';
+
 type Props = {
+    container: EntityContainer | null;
 }
 
-export const Container: React.FC<Props> = () => {
-    return (<></>);
+export const Container: React.FC<Props> = ({
+    container
+}) => {
+    if (container == null) return (null);
+
+    return (
+        <dl>
+            <dd>Name: {container.name}</dd>
+            <dd>ID: {container.id}</dd>
+            <dd>
+                <dl>
+                    <dt>Ports:</dt>
+                    {container.ports.map((port, index) => <dd key={index}>{port}</dd>)}
+                </dl>
+            </dd>
+            <dd>
+                <dl>
+                    <dt>Volumes:</dt>
+                    {container.volumes.map((volume, index) => <dd key={index}>{volume}</dd>)}
+                </dl>
+            </dd>
+        </dl>
+    );
 };
