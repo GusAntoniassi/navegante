4df55e262ec6dd9add16aa7e5e7f38e541bc9739
Gustavo Antoniassi <gus.antoniassi@gmail.com>
2020-05-03 18:34:29 -0300

    Add the core web folder to webpack watch

--- web/react/.rescriptsrc.js
+++ web/react/.rescriptsrc.js
@@ -1,4 +1,16 @@
+const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin');
+const path = require('path');
+
 module.exports = [
   ['use-babel-config', '.babelrc'],
   ['use-eslint-config', '.eslintrc.js'],
+
+  // add the 'core' folder to Webpack's watch mode
+  config => {
+    config.plugins.push(new ExtraWatchWebpackPlugin({
+      files: [ path.resolve('../core/index.ts') ],
+      dirs: [ path.resolve('../core/lib') ]
+    }));
+    return config;
+  }
 ];
