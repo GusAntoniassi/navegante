07f8617686517f0a016b8c556062b668027a6e34
Gustavo Antoniassi <gus.antoniassi@gmail.com>
2020-08-14 21:36:08 -0300

    Implement handler for retrieving a single user

--- api/handler/user.go
+++ api/handler/user.go
@@ -17,6 +17,10 @@ func MakeUserHandlers(r *mux.Router, n *negroni.Negroni, mgr user.Manager) {
 		negroni.Wrap(getAllUsers(mgr)),
 	)).Methods("GET", "OPTIONS")
 
+	r.Handle("/users/{id}", n.With(
+		negroni.Wrap(getUser(mgr)),
+	)).Methods("GET", "OPTIONS")
+
 	r.Handle("/users", n.With(
 		negroni.Wrap(addUser(mgr)),
 	)).Methods("POST")
@@ -51,6 +55,43 @@ func getAllUsers(mgr user.Manager) http.Handler {
 			log.Println("error converting users to JSON: ", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write(formatJSONError("Error converting users to JSON"))
+			return
+		}
+	})
+}
+
+func getUser(mgr user.Manager) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		idParam := vars["id"]
+		id, err := strconv.ParseUint(idParam, 10, 64)
+
+		if err != nil {
+			log.Println("error converting id param (", idParam, ") to uint64: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(formatJSONError("Invalid user ID received"))
+			return
+		}
+
+		usr, err := mgr.Get(user.ID(id))
+
+		// @TODO: Implement special case for user not found error
+		if err != nil {
+			log.Println("error calling mgr.Get: ", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(formatJSONError("Error getting user from the database"))
+			return
+		}
+
+		apiUser := apiEntity.User(*usr)
+
+		w.WriteHeader(http.StatusOK)
+		err = json.NewEncoder(w).Encode(apiUser)
+
+		if err != nil {
+			log.Println("error converting user to JSON: ", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(formatJSONError("Error converting user to JSON"))
 			return
 		}
 	})
